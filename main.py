3dd6b411cbc13c855c2d15a9ee8b538bf8ef9af8
nafestif <nafestog@gmail.com>
2025-07-30 05:00:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv(7733072125:AAHiDpo1tDCnsdSA4xQ2tpEz1VO-cE8ZXvM)
-ADMIN_USERNAME = os.getenv(@rrrrrkkkskksk)
+TOKEN = os.getenv("7733072125:AAHiDpo1tDCnsdSA4xQ2tpEz1VO-cE8ZXvM")
+ADMIN_USERNAME = os.getenv("@rrrrrkkkskksk")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"Привет, {update.effective_user.first_name}! Я бот 🤖")
